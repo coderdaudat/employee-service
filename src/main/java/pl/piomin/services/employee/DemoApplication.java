ebe490df554cb7b913c97abf3ccac89201da59ca
coderdaudat <coderdaudat@gmail.com>
2018-10-16 16:43:32 +0700

    jaeger and linkerd-zipkin

--- src/main/java/pl/piomin/services/employee/DemoApplication.java
+++ src/main/java/pl/piomin/services/employee/DemoApplication.java
@@ -1,8 +1,5 @@
 package pl.piomin.services.employee;
 
-import brave.Tracing;
-import brave.opentracing.BraveTracer;
-import brave.sampler.Sampler;
 import com.uber.jaeger.Configuration;
 import com.uber.jaeger.samplers.ProbabilisticSampler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,10 +15,6 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import zipkin.Span;
-import zipkin.reporter.AsyncReporter;
-import zipkin.reporter.Encoding;
-import zipkin.reporter.okhttp3.OkHttpSender;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -53,19 +46,19 @@ public class DemoApplication {
 				.getTracer();
 	}
 
-	@Bean
-	public io.opentracing.Tracer zipkinTracer() {
-		OkHttpSender okHttpSender = OkHttpSender.builder()
-				.encoding(Encoding.JSON)
-				.endpoint("http://35.240.176.82/api/v1/spans")
-				.build();
-		AsyncReporter<Span> reporter = AsyncReporter.builder(okHttpSender).build();
-		Tracing braveTracer = Tracing.newBuilder()
-				.localServiceName("employee-service")
-				.reporter(reporter)
-				.traceId128Bit(true)
-				.sampler(Sampler.ALWAYS_SAMPLE)
-				.build();
-		return BraveTracer.create(braveTracer);
-	}
+//	@Bean
+//	public io.opentracing.Tracer zipkinTracer() {
+//		OkHttpSender okHttpSender = OkHttpSender.builder()
+//				.encoding(Encoding.JSON)
+//				.endpoint("http://35.240.176.82/api/v1/spans")
+//				.build();
+//		AsyncReporter<Span> reporter = AsyncReporter.builder(okHttpSender).build();
+//		Tracing braveTracer = Tracing.newBuilder()
+//				.localServiceName("employee-service")
+//				.reporter(reporter)
+//				.traceId128Bit(true)
+//				.sampler(Sampler.ALWAYS_SAMPLE)
+//				.build();
+//		return BraveTracer.create(braveTracer);
+//	}
 }
