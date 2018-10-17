6dd9d266275479fee0f15bb072ad4ff500f9e8d2
coderdaudat <coderdaudat@gmail.com>
2018-10-17 17:30:29 +0700

    add zipkin

--- src/main/java/pl/piomin/services/employee/DemoApplication.java
+++ src/main/java/pl/piomin/services/employee/DemoApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 import pl.piomin.services.employee.repository.EmployeeRepository;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -18,6 +20,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableDiscoveryClient
 @EnableMongoRepositories
 @EnableSwagger2
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@EnableAsync
 public class DemoApplication {
 
 	public static void main(String[] args) {
@@ -37,46 +41,4 @@ public class DemoApplication {
 				.apiInfo(new ApiInfoBuilder().version("1.0").title("Employee API").description("Documentation Employee API v1.0").build());
 	}
 
-//	@Bean
-//	public io.opentracing.Tracer jaegerTracer() {
-//		Configuration config = Configuration.fromEnv();
-//
-//		System.out.println("Config_Host: " + config.getReporter().getSenderConfiguration().getAgentHost());
-//		System.out.println("Config_Port: " + config.getReporter().getSenderConfiguration().getAgentPort());
-//		System.out.println("Sampler_Host_Port: " + config.getSampler().getManagerHostPort());
-//
-//		return config.getTracer();
-//	}
-
-
-//	@Bean
-//	public io.opentracing.Tracer tracer() {
-//		return new Configuration("employee-service", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
-//						new Configuration.ReporterConfiguration())
-//						.getTracer();
-//	}
-
-
-//	@Bean
-//	public io.opentracing.Tracer jaegerTracer() {
-//		return new Configuration("employee-service", new Configuration.SamplerConfiguration(ProbabilisticSampler.TYPE, 1),
-//				new Configuration.ReporterConfiguration())
-//				.getTracer();
-//	}
-
-//	@Bean
-//	public io.opentracing.Tracer zipkinTracer() {
-//		OkHttpSender okHttpSender = OkHttpSender.builder()
-//				.encoding(Encoding.JSON)
-//				.endpoint("http://35.240.176.82/api/v1/spans")
-//				.build();
-//		AsyncReporter<Span> reporter = AsyncReporter.builder(okHttpSender).build();
-//		Tracing braveTracer = Tracing.newBuilder()
-//				.localServiceName("employee-service")
-//				.reporter(reporter)
-//				.traceId128Bit(true)
-//				.sampler(Sampler.ALWAYS_SAMPLE)
-//				.build();
-//		return BraveTracer.create(braveTracer);
-//	}
 }
